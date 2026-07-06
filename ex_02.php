9d4312c89d7f5be3adb69b424897bdaaa018d1b0
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-07-06 08:53:24 -0300

    Exercício 2 feito

---
+++ ex_02.php
@@ -0,0 +1,18 @@
+<?php
+
+function inverterTexto($texto)
+{
+    $invertido = strrev($texto);
+    $quantidade = strlen($texto);
+
+    echo "Texto original: $texto <br>";
+    echo "Texto invertido: $invertido <br>";
+    echo "Quantidade de caracteres: $quantidade";
+}
+
+$texto = "Programação";
+
+echo "<h3>Exercício 02</h3>";
+inverterTexto($texto);
+
+echo "<hr>";
