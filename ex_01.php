072565ad9ec52115f37b17b4a4c9c0dfe81227f2
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-07-08 12:19:34 -0300

    FIX - Exercício 1

--- ex_01.php
+++ ex_01.php
@@ -1,12 +1,19 @@
-<?php
+<? php
 
+//(x2+y2)/(x+y)
 function calcularFormula($x, $y)
 {
-    if (($x + $y) == 0) {
-        return "Não é possível realizar a divisão.";
+    if (($x+ $y) == 0) {
+        return "Não é possível realizar a divisão por zero.";
     }
+        $resultado = (pow($x, 2)+ pow($y, 2)) / ($x+ $y);
 
-    return (($x * 2) + $y * 2) / ($x + $y);
+        return $resultado;
 }
+$x = 10;
+$y = 5
 
-?>
+echo "Valor de X: $x <br>";
+echo "Valor de Y: $y <br><br>";
+echo "Resultado:
+: ". calcularFormula($x, $y);
